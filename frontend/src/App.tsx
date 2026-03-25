9d6ae4fa543aa5504631b82015015336a6300c89
Manikanta Thummuri <thummurimanikanta7@gmail.com>
2026-03-26 01:21:47 +0530

    frontend code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="p-10">
+      <Button>Hello shadcn 🎉</Button>
+    </div>
+  )
+}
+
+export default App
